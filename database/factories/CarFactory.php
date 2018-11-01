99c00865617c0fa89f7e76f54d12c85c30dabed2
Sayali Deshmane <sd796@njit.edu>
2018-11-01 15:22:39 -0700

    Feature: Created 3 attributes in CarFactory

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -1,9 +1,11 @@
 <?php
-
 use Faker\Generator as Faker;
-
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Car::class, function (Faker $faker) {
     return [
-        //
+        //'make' => $faker->firstName(),
+        'make' => $faker->randomElement(['firstName[]'=>'Ford', 'Honda', 'Toyota']),
+        'model' => $faker->company(),
+        'year' => $faker->date(),
     ];
+    //$faker = factory(App\User::class)->model(['name[]'=>'ford', 'honda', 'toyota']);
 });
